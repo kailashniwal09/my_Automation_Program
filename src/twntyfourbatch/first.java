b5d0a4c136f47cfebeafded9f69e38d2060c261a
kailashniwal09 <niwalkailash1204@outlook.com>
2024-05-29 17:21:17 +0530

    second commit

---
+++ src/twntyfourbatch/first.java
@@ -0,0 +1,12 @@
+package twntyfourbatch;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
